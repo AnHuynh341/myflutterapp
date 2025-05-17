f916fc7775edbd5586e774102a12ca562adaceca
AnHuynh341 <Meochon341@gmail.com>
2025-05-18 03:17:08 +0700

    initial com

---
+++ lib/assets/light.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:smart_control/assets/lighttab1.dart';
+import 'package:smart_control/assets/lighttab2.dart';
+
+class light extends StatefulWidget {
+  const light({Key? key}) : super(key: key);
+
+  @override
+  _lightPage createState() => _lightPage();
+}
+
+class _lightPage extends State<light>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    backgroundColor: Colors.white,
+    appBar: AppBar(
+      title: Text('Light'),
+    ),
+    body: SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.cyanAccent,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(5),
+                  child: TabBar(
+                    controller: tabController,
+                    unselectedLabelColor: Colors.white,
+                    labelColor: Colors.black,
+                    indicatorSize: TabBarIndicatorSize.tab,
+                    dividerColor: Colors.transparent,
+                    indicator: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    tabs: const [
+                      Tab(text: 'Tab 1'),
+                      Tab(text: 'Tab 2'),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TabBarView(
+                  controller: tabController,
+                  children: [
+                    lightTab1(),
+                    lightTab2(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+ }
+}
---
+++ lib/assets/lighttab1.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class lightTab1 extends StatelessWidget {
+  const lightTab1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('light tab 1')),
+    );
+  }
+}
